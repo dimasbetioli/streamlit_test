1ffbd4f4096dd05af6397c8bd95a4db0f66d1110
Dimas Betioli Ribeiro <69821362+dimasbetioli@users.noreply.github.com>
2024-12-18 16:07:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 # Estilo da sidebar
 with st.sidebar:
     st.image(
-        "https://raw.githubusercontent.com/dimasbetioli/streamlit_test/refs/heads/main/mult2.png",
+        "https://raw.githubusercontent.com/dimasbetioli/streamlit_test/refs/heads/main/mult.png",
         use_column_width=True,
     )
     st.markdown(
