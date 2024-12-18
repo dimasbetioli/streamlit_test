a6411a1bfb1153480f351ae37857fc4483fe9501
Dimas Betioli Ribeiro <69821362+dimasbetioli@users.noreply.github.com>
2024-12-18 15:42:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Teste de Tema",
-    page_icon="https://raw.githubusercontent.com/dimasbetioli/streamlit_test/refs/heads/main/mult.png",
+    page_icon=😃,
     layout="centered"
 )
 
